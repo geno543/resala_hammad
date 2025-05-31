3367980034fbb5fa9b53ab93a91a15f094ac19f2
Mohamed Fawzy Negm <157843257+Mohamedfawzynegm@users.noreply.github.com>
2025-05-31 15:26:17 +0300

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -179,7 +179,7 @@ const Contact = () => {
               <div className="rounded-xl overflow-hidden shadow-lg">
                 <iframe
                   title="موقع رسالة أبوحماد"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1234.5678!2d31.123456!3d30.123456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzDCsDA3JzM0LjQiTiAzMcKwMDcnMzQuNCJF!5e0!3m2!1sen!2seg!4v1234567890"
+                  src="https://maps.app.goo.gl/s8gWbu8TuXSV7Mo77"
                   width="100%"
                   height="250"
                   style={{ border: 0 }}
